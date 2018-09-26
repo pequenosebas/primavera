25c2bce267382491ffd78242b1284df07b1b9079
T-107 <T-107@PC205>
2018-09-25 21:45:23 -0500

    Ejercicio Tarjetas

--- src/main/java/unitec/primavera/PrimaveraApplication.java
+++ src/main/java/unitec/primavera/PrimaveraApplication.java
@@ -1,12 +1,21 @@
 package unitec.primavera;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class PrimaveraApplication {
+public class PrimaveraApplication implements CommandLineRunner{ //para ejecutar los system.out.ln
 
+    
 	public static void main(String[] args) {
 		SpringApplication.run(PrimaveraApplication.class, args);
+                
+                
 	}
+
+    @Override //En el run se debe de hacer todo lo de las tarjetas
+    public void run(String... args) throws Exception {
+        System.out.println("Hola Mundo"); //Hola Mundo en MongoDB
+    }
 }
